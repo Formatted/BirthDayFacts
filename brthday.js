1b14162710436b8db21562612ad854f364c9f784
formatted <Formatted@users.noreply.github.com>
2016-05-17 05:01:37 +0200

    create get API

--- brthday.js
+++ brthday.js
@@ -27,9 +27,54 @@ return returnDateTime.toString().substr(0,20);
 
 }
 
-var display_horoscope = function {
+var display_horoscope = function() {
 //pull horoscope from api
 //display fun fact
+// http://widgets.fabulously40.com/horoscope.json?sign=aries&date=2009-05-03
+var fixed = "http://widgets.fabulously40.com/horoscope.json?sign=";
+var sign = zodiac();
+var givenStr = $("#add_task").val(); // mm/dd/yyyy
+var month = givenStr.slice(0, 2); // /dd/yyyy
+var day = givenStr.slice(3, 5); // //yyyy
+var date = "&date=2009-" + Number(month) + "-" + Number(day);
+console.log(fixed + date);
 }
 
 //reason for conception???
+
+function zodiac() {
+
+	var givenStr = $("#add_task").val(); // mm/dd/yyyy
+	var month = givenStr.slice(0, 2); // /dd/yyyy
+	var day = givenStr.slice(3, 5); // //yyyy
+	var numerical_month = Number(month);
+	var numerical_day = Number(day);
+
+	var signs = ["Carpricorn", "Aquarius", "Pisces", "Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius"];
+
+	if((numerical_month == 1 && numerical_day <= 20) || (numerical_month == 12 && numerical_day >=22)) {
+    return signs[0];
+  } else if ((numerical_month == 1 && numerical_day >= 21) || (numerical_month == 2 && numerical_day <= 18)) {
+    return signs[1];
+  } else if((numerical_month == 2 && numerical_day >= 19) || (numerical_month == 3 && numerical_day <= 20)) {
+    return signs[2];
+  } else if((numerical_month == 3 && numerical_day >= 21) || (numerical_month == 4 && numerical_day <= 20)) {
+    return signs[3];
+  } else if((numerical_month == 4 && numerical_day >= 21) || (numerical_month == 5 && numerical_day <= 20)) {
+    return signs[4];
+  } else if((numerical_month == 5 && numerical_day >= 21) || (numerical_month == 6 && numerical_day <= 20)) {
+    return signs[5];
+  } else if((numerical_month == 6 && numerical_day >= 22) || (numerical_month == 7 && numerical_day <= 22)) {
+    return signs[6];
+  } else if((numerical_month == 7 && numerical_day >= 23) || (numerical_month == 8 && numerical_day <= 23)) {
+    return signs[7];
+  } else if((numerical_month == 8 && numerical_day >= 24) || (numerical_month == 9 && numerical_day <= 23)) {
+    return signs[8];
+  } else if((numerical_month == 9 && numerical_day >= 24) || (numerical_month == 10 && numerical_day <= 23)) {
+    return signs[9];
+  } else if((numerical_month == 10 && numerical_day >= 24) || (numerical_month == 11 && numerical_day <= 22)) {
+    return signs[10];
+  } else if((numerical_month == 11 && numerical_day >= 23) || (numerical_month == 12 && numerical_day <= 21)) {
+    return signs[11];
+  }
+}
